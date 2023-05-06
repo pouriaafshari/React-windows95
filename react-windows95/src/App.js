d3fbbd43559d066f7933785d23fa21a0625156d8
pouriaafshari <pooriyaafshari81@gmail.com>
2023-05-06 15:48:59 +0200

    Initiation and adding some elements from Vanilla version

---
+++ react-windows95/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Taskbar from './components/taskbar.js';
+
+import Taksbar from './components/taskbar.js'
+
+function App() {
+  return (
+    <>
+      <p>No</p>
+      <Taskbar />
+    </>
+  );
+}
+
+export default App;
---
+++ react-windows95/src/components/taskbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import StartIcon from '../img/windose.png';
+import SysIcon from '../img/system.png'
+
+export default function Taskbar()
+{
+
+    return(
+        <div id="taskbar">
+            <div id="start-button" onclick="StartButtonOpen()">
+                <img src={ StartIcon } alt="" />
+                <span>Start</span>
+            </div>
+            
+
+            <div id="tabs">
+                <div class="tab" id="welcome-tab">
+                    <img src={ SysIcon } alt="" />
+                    <span>Welcome</span>
+                </div>
+            </div>
+
+            <div id="clock">
+                <span id="time-text">6:15 PM</span>
+            </div>
+        </div>
+    );
+}
